0b59e0ea2074bbbc3a5bcaac8e2a05edf0b51811
Larissa Nguyen <larissa.nguyen@yale.edu>
2020-05-25 04:04:41 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -14,7 +14,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Steven", age: "17", attitude: "worried"},
         {name: "Benvolio", age: "17", attitude: "worried"},
       ]
    }
